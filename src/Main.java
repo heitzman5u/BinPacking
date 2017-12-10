547951a4ec549d0bccc70f4416ffe1bcb36a4455
heitzman5u <maureen.heitzmann@gmail.com>
2017-12-10 16:45:21 +0100

    BestFitPacking

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,12 @@ public class Main {
 		Algo algo1 = new Algo(Algo.FirstFitPacking, listeObj, 8);
 		System.out.println(algo1.getResultat());
 		System.out.println("\n");
+
+		// FRACTIONAL PACKING
+		System.out.println("BestFitPacking");
+		Algo algo2 = new Algo(Algo.BestFitPacking, listeObj, 8);
+		System.out.println(algo2.getResultat());
+		System.out.println("\n");
 		
 		//--
 	}
--- src/algos/Algo.java
+++ src/algos/Algo.java
@@ -8,6 +8,7 @@ public class Algo {
 
 	public static final int FractionalPacking = 0;
 	public static final int FirstFitPacking = 1;
+	public static final int BestFitPacking = 2;
 	
 	private int tailleSac;
 	private ArrayList<Integer> listeObj;
@@ -22,6 +23,9 @@ public class Algo {
 		}
 		if (algo == FirstFitPacking){
 			resultat = firstFitPacking();
+		}
+		if (algo == BestFitPacking){
+			resultat = bestFitPacking();
 		}
 	}
 	
@@ -82,13 +86,72 @@ public class Algo {
 		
 		for (int i = 0 ; i < listeSacs.size() ; i++){
 			System.out.println("sac " + i + " : "+ listeSacs.get(i).getTailleOccupee());
-		}
-		
-		
+		}	
 		
 		return listeSacs.size();	
 	}
 
+	
+	
+	
+	
+	
+	
+	private int bestFitPacking(){
+		ArrayList<Sac> listeSacs = new ArrayList<Sac>();
+		
+		for(int cptObj = 0 ; cptObj < listeObj.size() ; cptObj ++){
+			if (listeObj.get(cptObj) <= tailleSac){
+				
+				// Si on a des objets dans notre liste et que nous n'avons pas encore de sac, on en créé un.
+				if(listeSacs.size() == 0){
+					Sac sac = new Sac(tailleSac);
+					listeSacs.add(sac);
+				}
+				
+				// on recherche le 1er sac existant dont la capacité, apres l'ajout du sac, sera la plus grande.
+				boolean sacTrouve = false;
+				int maxCapaDispoSac = -1;
+				Sac sacMaxCapa = null;
+				
+				for (int i = 0 ; i < listeSacs.size() ; i++){
+					int capaDispoSac = listeSacs.get(i).getCapaciteMax() - listeSacs.get(i).getTailleOccupee();
+					
+					if (capaDispoSac >= listeObj.get(cptObj)){ //si l'obj rentre dans le sac alors ...
+						if(capaDispoSac - listeObj.get(cptObj) > maxCapaDispoSac ){ // je regarde si la capacité apres ajout est la plus gande parmis tous les sacs
+							
+							maxCapaDispoSac = capaDispoSac - listeObj.get(cptObj);
+							sacMaxCapa = listeSacs.get(i); 
+							
+						}
+					}
+				}
+				// si on a trouve un capable d'accueillir l'objet, on l'ajoute
+				if (maxCapaDispoSac != -1){
+					sacMaxCapa.ajouterObj(listeObj.get(cptObj));
+					sacTrouve = true;
+				}
+				
+				// si l'obj ne rentre dans aucun sac existant, on créer un nouveau sac et on le met dedans.
+				if (sacTrouve == false){
+					Sac sac = new Sac(tailleSac);
+					sac.ajouterObj(listeObj.get(cptObj));
+					listeSacs.add(sac);
+				}
+				
+			} else {
+				System.out.println("l'Objet est de taille superieure a la capacite max d'un sac (>"+tailleSac+")");
+				return -1;
+			}
+		}
+		
+		for (int i = 0 ; i < listeSacs.size() ; i++){
+			System.out.println("sac " + i + " : "+ listeSacs.get(i).getTailleOccupee());
+		}	
+		
+		return listeSacs.size();
+	}
+	
 
 	public int getResultat() {
 		return resultat;
